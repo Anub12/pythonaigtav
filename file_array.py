1d0952076dd8d09a44434fd5ffeed7fa415aed8b
Anubhav3380 <anubhav.gahlawat3380@gmail.com>
2024-04-21 16:56:19 +0530

    commit changes

--- file_array.py
+++ file_array.py
@@ -7,5 +7,17 @@ print(a.shape)
 print(a.dtype)
 print(a.size)
 
+a2 = np.arange(6)
+a2 = a[np.newaxis, :]
+a2.shape
+
+a = np.array([[1, 2, 3, 4],
+              [5, 6, 7, 8],
+              [9, 10, 11, 12]])
+
+print(a)
+print(a.shape)
+
+
 # for checking latest commit
 
